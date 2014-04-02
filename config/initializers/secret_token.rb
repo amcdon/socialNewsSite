81a67403437b1f4b88fb2d1f72bfd574e3df8bd1
Evan Duncan <evan@evan-desktop.(none)>
2014-04-02 17:22:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialNewsSite::Application.config.secret_key_base = '3f94e7a1754317b35ba6780e05918873a3352f8042315c033baa57a527ebff73ce0881e04238bdace1ad1f6fae40aa4333b20e2728ef53b2366eb84891152f0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialNewsSite::Application.config.session_store :cookie_store, key: '_socialNewsSite_session'
